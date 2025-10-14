86d303c505345158313f4875533ca3e34651b1cc
khantMinHtut <khantminhtut009@gmail.com>
2025-10-14 21:46:35 +0700

    fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import Hero from "@/components/Hero";
 
 export default function Home() {
-  return (
-    <Hero
-      heading={""}
-      description={""}
-      image={{
-        src: "",
-        alt: "",
-      }}
-    />
-  );
+  return <Hero />;
 }
